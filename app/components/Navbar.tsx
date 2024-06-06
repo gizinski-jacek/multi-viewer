79a3b86d0031d5ebe393d830066bfe203a6451b3
Jacek Gizinski <jacektrg+github@gmail.com>
2024-06-06 18:15:22 +0200

    Renaming video Source to Host across entire app.

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,21 +1,21 @@
-import { Sources } from '../libs/types';
+import { Hosts } from '../libs/types';
 import styles from './Navbar.module.scss';
 import { useEffect, useState } from 'react';
 
 interface Props {
-	addVideo: (source: Sources, userInput: string) => void;
-	toggleChat: (source: Sources, userInput: string) => void;
+	addVideo: (host: Hosts, userInput: string) => void;
+	toggleChat: (host: Hosts, userInput: string) => void;
 	activeChat: boolean;
 }
 
 export default function Navbar({ addVideo, toggleChat, activeChat }: Props) {
-	const [source, setSource] = useState<Sources>('youtube');
+	const [host, setHost] = useState<Hosts>('youtube');
 	const [userInput, setUserInput] = useState<string>('-FFyqea427M');
 	const [showNavbar, setShowNavbar] = useState<boolean>(true);
 
-	function handleSourceChange(e: React.ChangeEvent<HTMLSelectElement>) {
-		const { value } = e.target as { value: Sources };
-		setSource(value);
+	function handleHostChange(e: React.ChangeEvent<HTMLSelectElement>) {
+		const { value } = e.target as { value: Hosts };
+		setHost(value);
 	}
 
 	function handleUserInputChange(e: React.ChangeEvent<HTMLInputElement>) {
@@ -42,20 +42,22 @@ export default function Navbar({ addVideo, toggleChat, activeChat }: Props) {
 				onSubmit={(e) => e.preventDefault()}
 			>
 				<fieldset>
-					<label className='text-capitalize' htmlFor='source'>
-						Source
+					<label className='text-capitalize' htmlFor='host'>
+						Host
 					</label>
 					<select
 						className='ms-1 px-1'
-						id='source'
-						name='source'
-						value={source}
-						onChange={handleSourceChange}
+						id='host'
+						name='host'
+						value={host}
+						onChange={handleHostChange}
 					>
 						<option value=''>Select</option>
 						<option value='youtube'>Youtube</option>
 						<option value='twitch'>Twitch</option>
 						<option value='twitch-vod'>Twitch VOD</option>
+						<option value='dailymotion'>Dailymotion</option>
+						<option value='vimeo'>Vimeo</option>
 					</select>
 				</fieldset>
 				<fieldset>
@@ -74,7 +76,7 @@ export default function Navbar({ addVideo, toggleChat, activeChat }: Props) {
 				<button
 					className='btn btn-primary py-0 px-2 fw-bold text-uppercase'
 					type='button'
-					onClick={() => addVideo(source, userInput)}
+					onClick={() => addVideo(host, userInput)}
 				>
 					Go
 				</button>
@@ -82,7 +84,7 @@ export default function Navbar({ addVideo, toggleChat, activeChat }: Props) {
 			<button
 				className='btn btn-primary py-0 px-2 fw-bold text-uppercase'
 				type='button'
-				onClick={() => toggleChat(source, userInput)}
+				onClick={() => toggleChat(host, userInput)}
 				disabled={!activeChat}
 			>
 				Chat
