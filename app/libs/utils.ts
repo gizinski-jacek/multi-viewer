28a646cb8927addda9ae70570b11bd07e4f6f828
Jacek Gizinski <jacektrg+github@gmail.com>
2024-06-10 19:17:45 +0200

    Adding support for youtube playlists and dailymotion playlists.
    Adding function extracting video Id for every video hosting domain

--- app/libs/utils.ts
+++ app/libs/utils.ts
@@ -1,13 +1,38 @@
 import axios, { AxiosError, AxiosResponse } from 'axios';
 import { Hosts, VideoData } from './types';
 
-export const hostExtractURIList: { [key: string]: string } = {
-	youtube: 'https://www.youtube.com/watch?v=',
-	twitch: 'https://www.twitch.tv/',
-	'twitch-vod': 'https://www.twitch.tv/videos/',
-	dailymotion: 'https://www.dailymotion.com/video/',
-	vimeo: 'https://vimeo.com/',
-};
+export function extractVideoId(host: Hosts, string: string): string {
+	const str =
+		string[string.length - 1] === '/'
+			? string.slice(0, string.length - 1)
+			: string;
+	if (host === 'youtube') {
+		const id = str
+			.replace('https://youtu.be/', '')
+			.replace('https://www.youtube.com/watch?v=', '');
+		return id.slice(0, id.indexOf('?si='));
+	}
+	if (host === 'youtube-playlist') {
+		const id = str.slice(str.indexOf('?list=') + 6);
+		return id.slice(0, id.indexOf('?si='));
+	}
+	if (host === 'twitch') {
+		return str.slice(str.lastIndexOf('/') + 1);
+	}
+	if (host === 'twitch-vod') {
+		return str.slice(str.lastIndexOf('/') + 1);
+	}
+	if (host === 'dailymotion') {
+		return str.slice(str.lastIndexOf('/') + 1);
+	}
+	if (host === 'dailymotion-playlist') {
+		return str.slice(str.lastIndexOf('/') + 1);
+	}
+	if (host === 'vimeo') {
+		return str.slice(str.lastIndexOf('/') + 1);
+	}
+	throw new Error('Unsupported host or incorrect ID');
+}
 
 export async function getVideoData(
 	host: Hosts,
@@ -47,12 +72,16 @@ export function createIFrameVideoSource(host: Hosts, id: string): string {
 	switch (host) {
 		case 'youtube':
 			return `https://www.youtube-nocookie.com/embed/${id}`;
+		case 'youtube-playlist':
+			return `https://www.youtube.com/embed/videoseries?list=${id}`;
 		case 'twitch':
 			return `https://player.twitch.tv/?channel=${id}&parent=${embed_domain}`;
 		case 'twitch-vod':
 			return `https://player.twitch.tv/?video=${id}&parent=${embed_domain}`;
 		case 'dailymotion':
 			return `https://www.dailymotion.com/embed/video/${id}`;
+		case 'dailymotion-playlist':
+			return `https://www.dailymotion.com/embed/playlist/${id}`;
 		case 'vimeo':
 			return `https://player.vimeo.com/video/${id}`;
 		default:
