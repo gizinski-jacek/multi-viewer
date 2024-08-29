c228e4624b6bc62f79ca66a1318da4687acc0cff
Jacek Gizinski <jacektrg+github@gmail.com>
2024-08-29 20:20:39 +0200

    Extracting chat rendering logic to separate component.

---
+++ app/components/Chat.tsx
@@ -0,0 +1,48 @@
+import styles from './Chat.module.scss';
+import { VideoData } from '../libs/types';
+import { createIFrameChatSource } from '../libs/utils';
+import { IFrameWrapper } from './IFrameWrapper';
+
+interface Props {
+	videoData: VideoData[];
+	activeChat: VideoData | null;
+	changeChat: (data: VideoData) => void;
+}
+
+export default function Chat({ videoData, activeChat, changeChat }: Props) {
+	return (
+		<div className={styles.chat}>
+			{videoData.length ? (
+				<>
+					<div className={styles['chat-list']}>
+						{videoData.map(
+							(vid) =>
+								vid.livestreamChat && (
+									<div
+										className={`btn p-0 px-1 text-nowrap overflow-hidden ${
+											activeChat?.id === vid.id && activeChat?.host === vid.host
+												? 'btn-primary'
+												: 'btn-secondary'
+										}`}
+										key={vid.id}
+										onClick={() => changeChat(vid)}
+									>
+										{vid.channelName}
+									</div>
+								)
+						)}
+					</div>
+					<div className='flex-fill'>
+						{activeChat && (
+							<IFrameWrapper
+								src={createIFrameChatSource(activeChat.host, activeChat.id)}
+							/>
+						)}
+					</div>
+				</>
+			) : (
+				<div className='m-auto'>Add a video to see the chat</div>
+			)}
+		</div>
+	);
+}
