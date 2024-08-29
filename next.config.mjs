8bc3d971d898ef1d9987bd30aac05c3933c0c665
Jacek Gizinski <jacektrg+github@gmail.com>
2024-08-29 20:19:53 +0200

    Adding image hosting domains

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: '*.ytimg.com',
+				port: '',
+			},
+			{
+				protocol: 'https',
+				hostname: '*.static-cdn.jtvnw.net',
+				port: '',
+			},
+		],
+	},
+};
 
 export default nextConfig;
