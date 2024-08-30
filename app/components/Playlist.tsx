9032935b3cd638f91d5e5d0d54047e0cb2ce98a4
Jacek Gizinski <jacektrg+github@gmail.com>
2024-08-30 21:18:47 +0200

    Adding button to remove video from playlist.
    Adding placeholder if video's thumbnail is not present.

--- app/components/Playlist.tsx
+++ app/components/Playlist.tsx
@@ -1,35 +1,74 @@
+import styles from './Playlist.module.scss';
 import Image from 'next/image';
 import { VideoData } from '../libs/types';
-import styles from './Playlist.module.scss';
 
 interface Props {
 	navbarVisible: boolean;
 	playlist: VideoData[];
+	removeVideo: (video: VideoData) => void;
 }
 
-export default function Playlist({ navbarVisible, playlist }: Props) {
-	console.log(playlist);
+export default function Playlist({
+	navbarVisible,
+	playlist,
+	removeVideo,
+}: Props) {
 	return (
 		<div
 			style={{
 				height: navbarVisible ? 'calc(100vh - 80px)' : '100vh',
 			}}
-			className={`${styles.playlist} d-flex flex-column gap-2 p-2 bg-dark`}
+			className={styles.playlist}
 		>
-			{playlist.map((video) => (
-				<div key={video.id} className='d-flex gap-2'>
-					{video.thumbnailUrl && (
-						// !! Add placeholder if no thumbnail is present
-						<Image
-							src={video.thumbnailUrl}
-							width={100}
-							height={100}
-							alt='Video thumbnail'
-						/>
-					)}
-					<p>{video.title}</p>
-				</div>
-			))}
+			<div className={styles.container}>
+				{playlist.map((video, i) => (
+					<div key={video.id} className={styles.video}>
+						{video.thumbnailUrl ? (
+							<Image
+								src={video.thumbnailUrl}
+								width={100}
+								height={100}
+								alt='Video thumbnail'
+							/>
+						) : (
+							<div className={`${styles.placeholder} position-relative`}></div>
+						)}
+						<p>
+							<div
+								className={`${styles['remove-video']} btn btn-warning p-0 ms-1 mb-1`}
+								onClick={() => removeVideo(video)}
+							>
+								<svg
+									width='24px'
+									viewBox='0 0 24 24'
+									fill='none'
+									xmlns='http://www.w3.org/2000/svg'
+								>
+									<g strokeWidth='0'></g>
+									<g strokeLinecap='round' strokeLinejoin='round'></g>
+									<g>
+										<path
+											d='M7 17L16.8995 7.10051'
+											stroke='#000000'
+											strokeWidth='1.5'
+											strokeLinecap='round'
+											strokeLinejoin='round'
+										></path>
+										<path
+											d='M7 7.00001L16.8995 16.8995'
+											stroke='#000000'
+											strokeWidth='1.5'
+											strokeLinecap='round'
+											strokeLinejoin='round'
+										></path>
+									</g>
+								</svg>
+							</div>
+							{video.title}
+						</p>
+					</div>
+				))}
+			</div>
 		</div>
 	);
 }
