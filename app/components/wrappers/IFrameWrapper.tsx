1776ac7d814753f8677f95d9994b3b8f872c1b64
Jacek Gizinski <jacektrg+github@gmail.com>
2024-08-31 22:23:17 +0200

    Moving to 'wrappers' subdir

--- app/components/wrappers/IFrameWrapper.tsx
+++ app/components/wrappers/IFrameWrapper.tsx
@@ -1,10 +1,15 @@
-export function IFrameWrapper({ src }: { src: string }) {
+interface Props {
+	src: string;
+	title?: string;
+}
+
+export function IFrameWrapper({ src, title }: Props) {
 	return (
 		<iframe
 			width='100%'
 			height='100%'
 			src={src}
-			title='Twitch video player'
+			title={title || 'IFrame wrapper'}
 			allow='accelerometer; encrypted-media; gyroscope; picture-in-picture; web-share'
 			sandbox='allow-modals allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox allow-storage-access-by-user-activation'
 			referrerPolicy='strict-origin-when-cross-origin'
