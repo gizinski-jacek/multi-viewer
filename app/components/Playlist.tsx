8c624e0db20b03d9ae7e5dbd972acfcb0f054e4a
Jacek Gizinski <jacektrg+github@gmail.com>
2024-09-03 18:22:43 +0200

    Updating thumbnail size and alt attribute.
    Updating and cleaning up styling.
    Adding horizontal line between list items.

--- app/components/Playlist.tsx
+++ app/components/Playlist.tsx
@@ -24,85 +24,88 @@ export default function Playlist({
 		>
 			<div className={styles.container}>
 				{playlist.map((video, index) => (
-					<div key={video.id} className={styles.video}>
-						{video.thumbnailUrl ? (
-							<Image
-								src={video.thumbnailUrl}
-								width={100}
-								height={100}
-								alt='Video thumbnail'
-							/>
-						) : (
-							<div className={`${styles.placeholder} position-relative`}></div>
-						)}
-						<p className='flex-grow-1 m-0'>{video.title}</p>
-						<div className='d-flex flex-column gap-1 justify-content-between'>
-							<div
-								className={`${styles['remove-video']} btn btn-warning p-0 ms-1 mb-1`}
-								onClick={() => removeVideo(video)}
-							>
-								<svg
-									width='24px'
-									viewBox='0 0 24 24'
-									fill='none'
-									xmlns='http://www.w3.org/2000/svg'
+					<>
+						<div key={video.id} className={styles.video}>
+							{video.thumbnailUrl ? (
+								<Image
+									src={video.thumbnailUrl}
+									width={120}
+									height={90}
+									alt={`${video.title} thumbnail` || 'Video thumbnail'}
+								/>
+							) : (
+								<div className={`${styles.placeholder} position-relative`} />
+							)}
+							<p className='flex-grow-1 m-0'>{video.title}</p>
+							<div className='d-flex flex-column justify-content-between'>
+								<div
+									className='btn btn-danger p-0'
+									onClick={() => removeVideo(video)}
 								>
-									<g strokeWidth='0'></g>
-									<g strokeLinecap='round' strokeLinejoin='round'></g>
-									<g>
-										<path
-											d='M7 17L16.8995 7.10051'
-											stroke='#000000'
-											strokeWidth='1.5'
-											strokeLinecap='round'
-											strokeLinejoin='round'
-										></path>
-										<path
-											d='M7 7.00001L16.8995 16.8995'
-											stroke='#000000'
-											strokeWidth='1.5'
-											strokeLinecap='round'
-											strokeLinejoin='round'
-										></path>
-									</g>
-								</svg>
-							</div>
-							<div
-								className='btn btn-warning p-0 ms-1 mb-1'
-								onClick={() => reorderVideo(video, index - 1)}
-							>
-								<svg
-									width='24px'
-									fill='#000000'
-									viewBox='0 0 32 32'
-									xmlns='http://www.w3.org/2000/svg'
+									<svg
+										width='24px'
+										viewBox='0 0 24 24'
+										fill='none'
+										xmlns='http://www.w3.org/2000/svg'
+									>
+										<g strokeWidth='0'></g>
+										<g strokeLinecap='round' strokeLinejoin='round'></g>
+										<g>
+											<path
+												d='M7 17L16.8995 7.10051'
+												stroke='#000000'
+												strokeWidth='1.5'
+												strokeLinecap='round'
+												strokeLinejoin='round'
+											></path>
+											<path
+												d='M7 7.00001L16.8995 16.8995'
+												stroke='#000000'
+												strokeWidth='1.5'
+												strokeLinecap='round'
+												strokeLinejoin='round'
+											></path>
+										</g>
+									</svg>
+								</div>
+								<div
+									className='btn btn-warning p-0'
+									onClick={() => reorderVideo(video, index - 1)}
 								>
-									<g strokeWidth='0'></g>
-									<g strokeLinecap='round' strokeLinejoin='round'></g>
-									<g>
-										<path d='M8 20.695l7.997-11.39L24 20.695z'></path>
-									</g>
-								</svg>
-							</div>
-							<div
-								className='btn btn-warning p-0 ms-1 mb-1'
-								onClick={() => reorderVideo(video, index + 1)}
-							>
-								<svg
-									width='24px'
-									fill='#000000'
-									viewBox='0 0 32 32'
-									xmlns='http://www.w3.org/2000/svg'
+									<svg
+										width='24px'
+										fill='#000000'
+										viewBox='0 0 32 32'
+										xmlns='http://www.w3.org/2000/svg'
+									>
+										<g strokeWidth='0'></g>
+										<g strokeLinecap='round' strokeLinejoin='round'></g>
+										<g>
+											<path d='M8 20.695l7.997-11.39L24 20.695z'></path>
+										</g>
+									</svg>
+								</div>
+								<div
+									className='btn btn-warning p-0'
+									onClick={() => reorderVideo(video, index + 1)}
 								>
-									<g strokeWidth='0'></g>
-									<g strokeLinecap='round' strokeLinejoin='round'></g>
-									<g>
-										<path d='M24 11.305l-7.997 11.39L8 11.305z'></path>
-									</g>
-								</svg>
+									<svg
+										width='24px'
+										fill='#000000'
+										viewBox='0 0 32 32'
+										xmlns='http://www.w3.org/2000/svg'
+									>
+										<g strokeWidth='0'></g>
+										<g strokeLinecap='round' strokeLinejoin='round'></g>
+										<g>
+											<path d='M24 11.305l-7.997 11.39L8 11.305z'></path>
+										</g>
+									</svg>
+								</div>
 							</div>
 						</div>
-					</div>
+						{index !== playlist.length - 1 && <hr />}
+					</>
 				))}
 			</div>
 		</div>
